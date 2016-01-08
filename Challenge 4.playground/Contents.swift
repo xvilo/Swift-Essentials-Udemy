372375c8fe2c0ff98c9918a78a8876c2f87602c3
sem schilder <sem.schilder@gmail.com>
2016-01-08 20:19:26 +0100

    comments and print

--- Challenge 4.playground/Contents.swift
+++ Challenge 4.playground/Contents.swift
@@ -1,2 +1,20 @@
 // Challenge 4
 
+//1
+//done
+
+//2
+/*
+wow
+multi
+line
+thingy
+*/
+
+//3
+
+var string : String = "you ass"
+var double : Double = 2.7
+var int : Int = 1
+
+print("This is cool, we can print an Double \(double) an int: \(int) and of course a string (around, \(string))")
