f94b7ca1699c27e52c974246fb8611e1c8ec9357
sem schilder <sem.schilder@gmail.com>
2016-01-08 20:58:57 +0100

    booleans and if statements

---
+++ Booleans and if statements.playground/Contents.swift
@@ -0,0 +1,49 @@
+//bool
+
+let leeftijd : Int = 20
+let name : String = "Sem"
+let rating : Double = 4.3
+
+var levend : Bool = true
+
+//if statements
+let payday = true
+
+var wallet = 40
+
+wallet = wallet - 5
+
+if payday {
+    wallet = wallet + 200
+}
+
+wallet = wallet - 10
+
+if wallet <= 225 {
+    print("Yes, wallet is \(wallet)!")
+}
+
+//challenge
+
+//1
+var boolean = true
+
+//2
+let bool = false
+
+let age = 20
+
+if age >= 20 {
+    print("You are an adult!")
+}else{
+    print("You are a minor")
+}
+
+
+let naam = "yolo"
+
+if naam == "sem" || naam == "poop"{
+    print("Yes! Name is \(naam)")
+}else{
+    print("nope not right")
+}
