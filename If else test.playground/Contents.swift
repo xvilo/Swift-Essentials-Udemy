5d3e3b5acb6e0b79c41fd8ddcbf32e524179ab2e
sem schilder <sem.schilder@gmail.com>
2016-01-08 20:03:29 +0100

    If else, strings ints and challenge

---
+++ If else test.playground/Contents.swift
@@ -0,0 +1,12 @@
+// if else test
+
+let mainValue = 10
+var userInput = "test"
+
+userInput = "test"
+
+if userInput === mainValue {
+    print("Gelijk: \(userInput)")
+}else{
+    print("Ongelijk: \(userInput)")
+}
--- Strings and ints.playground/Contents.swift
+++ Strings and ints.playground/Contents.swift
@@ -1,7 +1,34 @@
-let firstName = "Sem"
-let lastname = "Schilder"
-var fullname = firstName + " " + lastname
 
-fullname = fullname.lowercaseString
+var hondNaam = "Gompie"
 
-fullname
+var hondAge = 8
+
+var hondInfo = "Mijn hond, \(hondNaam) is \(hondAge) jaar oud. En hij is saardig :D"
+
+let yepRating : Float = 4.32342423423423452438975239845792037549027354972394587023975908237540927354
+
+let ftPercent : Double = 4.32342423423423452438975239845792037549027354972394587023975908237540927354
+
+
+//challenge #2
+
+//1
+var stringVar = "String variable"
+
+//2
+var stringInt = 3
+
+//3
+let verhaaltje = "Gelukkig is dit een \(stringVar) en dan is '\(stringInt)' een int variable."
+
+//4
+stringVar = "stukje text"
+stringInt = 4
+
+//
+var varIntDouble : Double = 4.5
+
+var varDivision = Double(stringInt) / varIntDouble;
+
+
+
