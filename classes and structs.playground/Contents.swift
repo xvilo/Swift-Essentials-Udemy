026d3597f00e40792b72ed446aa2b1de32023052
Sem Schilder <me@sem.design>
2016-02-27 21:10:22 +0000

    first video

--- classes and structs.playground/Contents.swift
+++ classes and structs.playground/Contents.swift
@@ -1,1 +1,26 @@
 //classes and structs
+
+//declaring a class
+class Dog {
+    
+    //properties
+    var name = ""
+    var furColour = ""
+    var age = 0
+    
+    
+}
+
+//using classes
+var fido = Dog()
+fido.name = "Fido"
+fido.age = 20
+fido.furColour = "Brown"
+
+var dog2 = Dog()
+dog2.name = "Jane"
+dog2.age = 2
+dog2.furColour = "White"
+
+var dogs : [Dog] = [fido, dog2]
+
