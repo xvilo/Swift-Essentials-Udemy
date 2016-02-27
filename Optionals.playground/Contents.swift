bd7462235480d48b6a0b8c20e6663f9dd3922614
Sem Schilder <me@sem.design>
2016-02-27 20:28:13 +0000

    exclamation points

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -6,5 +6,9 @@ let rating:Double? = nil
 
 let Thenumber = Int("234245")
 print(Thenumber) //becomes an optional
+print(Thenumber!) //becomes an Int
+
+print(name) // becomes an optional
+print(name!) // becomes String
 
 
