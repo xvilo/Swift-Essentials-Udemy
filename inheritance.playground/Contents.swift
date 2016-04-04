de8adb2f4903d5c977a78f01aa03abaf429aa44e
Sem Schilder <me@sem.design>
2016-04-04 13:58:36 +0100

    done.

--- inheritance.playground/Contents.swift
+++ inheritance.playground/Contents.swift
@@ -1,5 +1,66 @@
-//: Playground - noun: a place where people can play
+//Inheritance
 
-import UIKit
+//subclasses
 
-var str = "Hello, playground"
+class Animal{
+    var name = ""
+    var age = 0
+    
+    func makeNoise(){
+        print("SWUGUUIBWUIBWGRU")
+    }
+    
+    func description() -> String{
+        return "Animal"
+    }
+}
+
+class Mammal : Animal{
+    var hairColor = ""
+    
+    override func makeNoise(){
+        print("ROAARROOWORORJOHNASDF")
+    }
+    
+    override func description() -> String{
+        return super.description() + " - Mammal"
+    }
+}
+
+class Monkey : Mammal{
+    var thumbSize = 0
+    
+    override func makeNoise(){
+        print("OOHH OHHH OOHH AAAH AAAH AAAAAAH")
+    }
+    
+    override func description() -> String{
+        return super.description() + " (Monkey)"
+    }
+}
+
+var snake = Animal()
+snake.age = 10;
+snake.name = "Sam"
+
+
+var dog = Mammal()
+dog.age = 5
+dog.name = "Fido"
+//wow
+dog.hairColor = "Brown"
+
+var george = Monkey()
+george.age = 5
+george.name = "George"
+//wow
+george.hairColor = "Brown"
+//wooot
+george.thumbSize = 10
+
+//overriding
+george.makeNoise()
+print("Desc: " + george.description())
+
+//Final
+//just type 'final' before a function if you want to 'protect' it against overriding. 
