e5cc52c5e1d04a6156d17dc971217c49d9cbb72d
Sem Schilder <me@sem.design>
2016-04-04 13:38:26 +0100

    properties challenge

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -41,5 +41,26 @@ class Owner {
 var sem = Owner()
 
 //Challenge
+import Darwin // or Foundation or UIKit
 
-//1 
+class Pizza{
+    var radius : Double = 0{
+        willSet(newRadius){
+            if newRadius > radius{
+                print("The pizza is getting bigger")
+            }else{
+                print("The pizza is getting smaller")
+            }
+        }
+    }
+    let π = M_PI
+    var size : Double {
+        return(π * radius * radius);
+    }
+    
+}
+
+var myPizza = Pizza();
+
+myPizza.radius = 10.00;
+print("The size is: \(myPizza.size)")
