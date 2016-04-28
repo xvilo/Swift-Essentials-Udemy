2ca1b9ba97f143940b852935e23f68054bfb5d8c
Sem Schilder <me@sem.design>
2016-04-28 19:35:06 +0100

    inits

--- Initialisation.playground/Contents.swift
+++ Initialisation.playground/Contents.swift
@@ -1,5 +1,63 @@
-//: Playground - noun: a place where people can play
+//initialisation
 
-import UIKit
+class Dog {
+    var name : String
+    var age = 0
+    
+    init(name: String){
+        print("Object created")
+        self.name = name
+    }
+    
+    init?(name: String, age: Int){
+        //failable initializer
+        print("Object created")
+        self.name = name
+        self.age = age
+        
+        if name.isEmpty{
+            return nil
+        }
+        
+        if age < 0{
+            return nil
+        }
+    }
+    
+    deinit{
+        print("Object deleted")
+    }
+}
 
-var str = "Hello, playground"
+var myDog = Dog(name: "Fido", age: 7)
+
+myDog = nil
+
+//Challenge
+
+//1 
+class Quote{
+    let author : String
+    let text : String
+    
+    init?(text:String){
+        author = "Unkown"
+        self.text = text
+        if author.isEmpty{
+            return nil
+        }
+    }
+    
+    init?(author:String, text:String){
+        self.author = author
+        self.text   = text
+        
+        if self.author.isEmpty || self.text.isEmpty {
+            return nil
+        }
+    }
+    
+    deinit{
+        print("Goodbye!")
+    }
+}
