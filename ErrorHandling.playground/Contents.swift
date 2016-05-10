4c31f693a6d5c55f1ae4386ea333032c5f736f18
Sem Schilder <me@sem.design>
2016-05-10 14:21:04 +0100

    error save

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -1,1 +1,24 @@
 // error handling
+
+enum DogError : ErrorType{
+    case EmptyString
+    case NegativeAge
+}
+
+//throwing errors
+
+class Dog {
+    var name = ""
+    var age = 0
+    
+    func updateName(name: String) throws -> String{
+        if name.isEmpty{
+            throw DogError.EmptyString
+        }
+        self.name = name
+        return name
+    }
+}
+
+//Error handling
+var theDog = Dog()
