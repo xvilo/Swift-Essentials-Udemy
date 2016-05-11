fd12139971bb752e32b0787e73da012cd388aa72
Sem Schilder <me@sem.design>
2016-05-11 14:45:40 +0100

    extensions

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -19,7 +19,7 @@ extension String {
 
 String(string: "Hi ", repeated: 20)
 
-String(string: "Whoohoo", repeated: 50).printIt()
+String(string: "Whoohoo ", repeated: 50).printIt()
 
 extension Int {
     mutating func doubleIt(){
@@ -30,3 +30,49 @@ extension Int {
 var age = 4
 age.doubleIt()
 age
+
+//Challenge
+
+//1
+extension Double{
+    var timesTwo:Double{
+       return self * 2
+    }
+}
+
+Double(2.66).timesTwo
+
+//2
+extension Bool {
+    init() {
+        self = true
+    }
+}
+
+var yesOrNo = true
+
+//3
+extension String{
+    func printItMany(times:Int){
+        for _ in 1...times{
+            print(self)
+        }
+    }
+}
+String("hi").printItMany(3)
+
+//4
+
+extension Int{
+    mutating func addUp(amount:Int){
+        self = self * amount
+    }
+}
+
+var amount = 5
+amount.addUp(10)
+print(amount)
+
+
+
+
